67540a4f43c92bde39ccd21e4bb267a8c827a67f
Luuk Hendriks <mail@luukhendriks.eu>
2020-03-20 17:29:13 +0100

    parse and verify first part of .mft's

--- src/RPKI.jl
+++ src/RPKI.jl
@@ -544,6 +544,115 @@ function check(o::RPKIObject{CER}) :: RPKIObject{CER}
     
     o
 end
+
+function check_signed_data(o::RPKIObject{MFT}, sd::Node) :: RPKIObject{MFT}
+    @debug "check_signed_data for a ", sd
+
+    # Signed-Data as defined in RFC 5652 (CMS) can contain up to 6 children
+    # but for RPKI Manifests, we MUST have the CertificateSet and MUST NOT have
+    # the RevocationInfoChoices
+    checkchildren(sd, 5)
+
+    # CMSVersion
+    tagvalue(sd[1], ASN.INTEGER, 0x03)
+
+    # DigestAlgorithmIdentifiers
+    tagisa(sd[2], ASN.SET) 
+    tagisa(sd[2,1], ASN.SEQUENCE)
+    tagvalue(sd[2,1,1], ASN.OID, "2.16.840.1.101.3.4.2.1")
+    tagisa(sd[2,1,2], ASN.NULL)
+
+    # EncapsulatedContentInfo
+    tagisa(sd[3], ASN.SEQUENCE)
+    eContentType = sd[3,1]
+    eContent = sd[3,2]
+
+    tagvalue(eContentType, ASN.OID, "1.2.840.113549.1.9.16.1.26")
+    tagis_contextspecific(eContent, 0x00)
+    tagisa(eContent[1], ASN.OCTETSTRING)
+
+    # now, be flexible: for BER mft's, there will be another OCTETSTRING
+    # either way, the eContent only has 1 child
+    checkchildren(eContent[1], 1)
+    manifest = if eContent[1,1].tag isa Tag{ASN.OCTETSTRING} #TODO make a function for this
+        remark!(eContent[1,1], "nested OCTETSTRING, BER instead of DER")
+        # we need to do a second pass on this then
+        DER.parse_append!(DER.Buf(eContent[1,1].tag.value), eContent[1,1])
+        eContent[1,1,1]
+    elseif eContent[1,1] isa Tag{ASN.SEQUENCE}
+        eContent[1,1]
+    else
+        remark!(eContent[1,1], "unexpected tag $(tagtype(eContent[1,1]))")
+        return o
+    end
+    @debug "Manifest is: ", manifest
+    o = check_manifest(o, manifest)
+
+    # CertificateSet, optional [0] (IMPLICITly tagged), MUST be here
+    tagis_contextspecific(sd[4], 0x00)
+
+
+    o
+end
+
+function check_manifest(o::RPKIObject{MFT}, m::Node) :: RPKIObject{MFT}
+    #RFC 6486
+    tagisa(m, ASN.SEQUENCE)
+    checkchildren(m, 5:6)
+    # the 'version' is optional, defaults to 0
+    offset = 0
+    if length(m.children) == 6
+        offset = 1
+        # version:
+        tagis_contextspecific(m[1], 0x00)
+        # EXPLICIT tagging, so the version must be in a child
+        checkchildren(m[1], 1)
+        tagisa(m[1, 1], ASN.INTEGER)
+        if value(m[1, 1].tag) == 0
+            remark!(m[1, 1], "version explicitly set to 0 while that is the default")
+        end
+    end
+
+    # manifestNumber
+    tagisa(m[offset+1], ASN.INTEGER)
+
+    # TODO: attach these to the RPKIObject.object ?
+    # thisUpdate
+    tagisa(m[offset+2], ASN.GENTIME) 
+    # nextUpdate
+    tagisa(m[offset+3], ASN.GENTIME) 
+
+    # fileHashAlg
+    tagvalue(m[offset+4], ASN.OID, "2.16.840.1.101.3.4.2.1")
+
+    # fileList
+    filelist = m[offset+5]
+    tagisa(filelist, ASN.SEQUENCE)
+    for file_and_hash in filelist.children
+        tagisa(file_and_hash, ASN.SEQUENCE)
+        tagisa(file_and_hash[1], ASN.IA5STRING)
+        tagisa(file_and_hash[2], ASN.BITSTRING)
+        @debug file_and_hash[2]
+    end
+
+    o
+end
+
+function check(o::RPKIObject{MFT}) :: RPKIObject{MFT}
+    cmsobject = o.tree
+    # CMS, RFC5652
+    tagisa(o.tree, ASN.SEQUENCE)
+    tagvalue(o.tree[1], ASN.OID, "1.2.840.113549.1.7.2") # contentType
+    tagis_contextspecific(o.tree[2], 0x00) # content
+
+    # 6488:
+    tagisa(o.tree[2, 1], ASN.SEQUENCE)
+    o = check_signed_data(o, o.tree[2, 1])
+    
+    o
+end
+
+
 function check(::RPKIObject{T}) where {T}
     @warn "unknown RPKIObject type"
 end

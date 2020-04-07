fd5875d1d2163efc3517580a022891e708be622f
Luuk Hendriks <mail@luukhendriks.eu>
2020-04-07 13:19:14 +0200

    split up JSONHelpers in separate file, implement objects endpoint

---
+++ src/JSONHelpers.jl
@@ -0,0 +1,95 @@
+JSON2.@format RPKI.RPKINode begin
+        parent => (;exclude=true,)
+end
+
+JSON2.@format RPKI.RPKIObject{T} where T begin
+        tree => (;exclude=true,)
+end
+
+# custom view for RPKIObject{T}
+# includes the tree, but does not link to parent or children
+# howto: https://github.com/quinnj/JSON2.jl/issues/12
+struct ObjectDetails{T}
+    filename::String
+    tree::RPKI.Node
+    object::T
+    objecttype::String
+    remarks::Union{Nothing, Vector{RPKI.Remark}}
+end
+ObjectDetails(r::RPKI.RPKIObject) = ObjectDetails(r.filename,
+                                        r.tree,
+                                        r.object,
+                                        string(nameof(typeof(r.object))),
+                                        r.remarks)
+
+struct Basename
+    filename::String
+end
+JSON2.@format ObjectDetails begin
+    filename => (; jsontype=Basename,)
+end
+JSON2.write(io::IO, bn::Basename) = JSON2.write(io, basename(bn.filename))
+Base.convert(Basename, s) = Basename(s)
+
+function JSON2.write(io::IO, t::ASN.Tag{T}) where {T}
+    JSON2.write(io, "$(nameof(ASN.tagtype(t))) ($(t.len))")
+    #JSON2.write(io, " ($(t.len))")
+end
+
+
+# Slim copy of RPKI.CER, with empty prefixes and ASNs Vectors
+struct SlimCER 
+    pubpoint::String
+    manifest::String
+    rrdp_notify::String
+    inherit_prefixes::Bool
+    prefixes::Vector{Union{IPNet, Tuple{IPNet, IPNet}}}
+    inherit_ASNs::Bool
+    ASNs::Vector{Union{Tuple{UInt32, UInt32}, UInt32}}
+end
+SlimCER(cer::RPKI.CER) = SlimCER(cer.pubpoint, cer.manifest, cer.rrdp_notify, cer.inherit_prefixes, [], cer.inherit_ASNs, [])
+JSON2.@format SlimCER begin
+    prefixes => (;exclude=true,)
+    ASNs => (;exclude=true,)
+end
+
+# Slim copy of RPKI.MFT, with an empty files Vector
+struct SlimMFT
+    files::Vector{String}
+    loops::Union{Nothing, Vector{String}}
+    missing_files::Union{Nothing, Vector{String}}
+    this_update::Union{Nothing, DateTime}
+    next_update::Union{Nothing, DateTime}
+end
+SlimMFT(mft::RPKI.MFT) = SlimMFT([], mft.loops, mft.missing_files, mft.this_update, mft.next_update)
+JSON2.@format SlimMFT begin
+    files => (;exclude=true,)
+end
+
+
+to_slim(o::RPKI.MFT) = SlimMFT(o)
+to_slim(o::RPKI.CER) = SlimCER(o)
+to_slim(o::RPKI.ROA) = o
+
+to_slim(o::RPKI.RPKIObject) = RPKI.RPKIObject(o.filename, o.tree, to_slim(o.object), o.remarks)
+
+# the circular ref between  `parent` and `children` in the RPKINode struct
+# causes the JSON generation to stackoverflow
+# So we combine to_root() with a custom JSON format where we exclude the parent
+# Results are then a chain from root to leaf, though without refs to the parent
+# FIXME this comment is outdated/incorrect now
+function to_root(node::RPKI.RPKINode) :: Vector
+    current = node
+    res = Vector{Any}([current.obj])
+    while !isnothing(current.parent)
+        if !isnothing(current.parent.obj)
+            #push!(res, to_slim(current.parent.obj.object))
+            push!(res, to_slim(current.parent.obj))
+        end
+        current = current.parent
+    end
+    res
+end
+
+
+
--- src/Webservice.jl
+++ src/Webservice.jl
@@ -2,6 +2,8 @@ using HTTP
 using Dates
 using JSON2
 
+using IPNets
+
 using JuliASN
 
 const ROUTER = HTTP.Router()
@@ -10,23 +12,49 @@ const LAST_UPDATE = Ref(now(UTC))
 const LAST_UPDATE_SERIAL = Ref(0)
 const LOOKUP = Ref(RPKI.Lookup())
 
+include("JSONHelpers.jl")
+
+const TAL_URLS = Dict(
+    :afrinic    => "rsync://rpki.afrinic.net/repository/AfriNIC.cer",
+    :apnic      => "rsync://rpki.apnic.net/repository/apnic-rpki-root-iana-origin.cer",
+    :arin       => "rsync://rpki.arin.net/repository/arin-rpki-ta.cer",
+    :lacnic     => "rsync://repository.lacnic.net/rpki/lacnic/rta-lacnic-rpki.cer",
+    :ripe       => "rsync://rpki.ripe.net/ta/ripe-ncc-ta.cer",
+    :ripetest   => "rsync://localcert.ripe.net/ta/RIPE-NCC-TA-TEST.cer",
+    :apnictest  => "rsync://rpki-testbed.apnic.net/repository/apnic-rpki-root-iana-origin-test.cer"
+)
 
 function asn(req::HTTP.Request)
-    asid = HTTP.URIs.splitpath(req.target)[4] # /api/v1/asn/10, get 10
-    @info "returning for asn()"
-    return "fake results for ASN $(asid)"
+    # /api/v1/asn/10, get 10
+    asid = HTTP.URIs.splitpath(req.target)[4] 
+    res = RPKI.lookup(LOOKUP[], RPKI.AutSysNum(parse(UInt32, asid)))
+    return [to_root(r) for r in res] 
 end
 
 function prefix(req::HTTP.Request)
-    prefix = HTTP.URIs.splitpath(req.target)[4] # /api/v1/prefix/10, get 10
+    # /api/v1/prefix/1.2.3.4%2F24, get and unescape prefix
+    prefix = HTTP.URIs.splitpath(req.target)[4] 
     prefix = HTTP.URIs.unescapeuri(prefix)
-    return "fake results for prefix $(prefix)"
+    prefix = IPNet(prefix)
+    res = RPKI.lookup(LOOKUP[], prefix)
+    return [to_root(r) for r in res]
+end
+
+function object(req::HTTP.Request)
+    # /api/v1/object/escaped_filename, get and unescape filename
+    object = HTTP.URIs.splitpath(req.target)[4] 
+    object = HTTP.URIs.unescapeuri(object)
+    res = RPKI.lookup(LOOKUP[], object)
+    # res is a Vector or nothing
+    res = [ObjectDetails(r.obj) for r in res]
+    @debug res
+    res
 end
 
 
 function update()
     @info "update()"
-    (_, lookup) = fetch(Threads.@spawn(RPKI.retrieve_all()))
+    (_, lookup) = fetch(Threads.@spawn(RPKI.retrieve_all(TAL_URLS)))
     LOOKUP[] = lookup
     set_last_update()
     @info "update() done, serial: $(LAST_UPDATE_SERIAL[])"
@@ -39,6 +67,7 @@ end
 function _init()
     HTTP.@register(ROUTER, "GET", APIV*"/asn/*", asn)
     HTTP.@register(ROUTER, "GET", APIV*"/prefix/*", prefix)
+    HTTP.@register(ROUTER, "GET", APIV*"/object/*", object)
     HTTP.@register(ROUTER, "GET", APIV*"/update", update)
 end
 
@@ -50,7 +79,7 @@ struct Envelope
 end
 
 function set_last_update()
-    LAST_UPDATE[] = now(UTC) + Hour(6)
+    LAST_UPDATE[] = now(UTC)
     LAST_UPDATE_SERIAL[] += 1
 end
 
@@ -69,7 +98,10 @@ function JSONHandler(req::HTTP.Request)
     # wrap the response in an envelope
     #return HTTP.Response(200, JSON2.write(response_body))
     response = Envelope(LAST_UPDATE[], LAST_UPDATE_SERIAL[], now(UTC), response_body)
-    return HTTP.Response(200, JSON2.write(response))
+    return HTTP.Response(200,
+                         [("Content-Type" => "application/json")];
+                         body=JSON2.write(response)
+                        )
 end
 
 function updater()
@@ -86,7 +118,7 @@ function start()
     @info "init() ..."
     _init()
     @info "init() done" 
-
+    @async updater()
     HTTP.serve(JSONHandler, "::1", 8081)
 end
 

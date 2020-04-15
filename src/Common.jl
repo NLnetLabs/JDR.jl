41f08bb0fdd89238f3a91d6ce47f80eea7904277
Luuk Hendriks <mail@luukhendriks.eu>
2020-04-15 11:30:15 +0200

    add missing Common.jl

---
+++ src/Common.jl
@@ -0,0 +1,69 @@
+module Common
+export Remark, RemarkLevel, RemarkCounts, RemarkCounts_t, count_remarks
+export remark!, dbg!, info!, warn!, err!
+
+@enum RemarkLevel DBG INFO WARN ERR
+
+struct Remark
+    lvl::RemarkLevel
+    msg::String
+end
+
+## Example:
+#struct MyObject
+#    id::Integer
+#    remarks::Vector{Remark}
+#    remark_counts::Dict{Level, Integer}
+#end
+#MyObject(i::Integer) = MyObject(i, [], RemarkCounts())
+
+
+# Helper for constructors:
+const RemarkCounts_t = Dict{RemarkLevel, Int64}
+RemarkCounts() = Dict(lvl => 0 for lvl in instances(RemarkLevel)) 
+
+function remark!(o::Any, lvl::RemarkLevel, msg::String)
+	if isnothing(o.remarks)
+        o.remarks = Vector{Remark}([])
+	end	
+	push!(o.remarks, Remark(lvl, msg))
+end
+
+dbg!(o::Any, msg::String) 	= remark!(o, DBG, msg)
+info!(o::Any, msg::String) 	= remark!(o, INFO, msg)
+warn!(o::Any, msg::String) 	= remark!(o, WARN, msg)
+err!(o::Any, msg::String)  	= remark!(o, ERR, msg)
+
+function remark!(o::Any, msg::String)
+    @warn "common.jl: remark!() is deprecated! defaulting to info!()" maxlog=10
+    info!(o, msg)
+end
+
+function _count_remarks!(o::Any)
+	@assert reduce(+, values(o.remarks)) == 0
+    for r in o.remarks
+        o.remark_counts[r.lvl] += 1
+    end
+    o.remark_counts
+end
+
+function count_remarks(o::T) where {T<:Any}
+    res = RemarkCounts()
+    if !isnothing(o.remarks)
+        for r in o.remarks
+            res[r.lvl] += 1
+        end
+    end
+    res
+end
+
+import Base.+
+function +(c1::RemarkCounts_t, c2::RemarkCounts_t) :: RemarkCounts_t
+    res = RemarkCounts()
+    for lvl in instances(RemarkLevel)
+        res[lvl] = c1[lvl] + c2[lvl]
+    end
+    res
+end
+
+end # module

4cbcd24a8c50cfc3062ae9adcdb4f9c47621061b
Luuk Hendriks <mail@luukhendriks.eu>
2020-09-25 10:42:45 +0200

    implement iterate() for PrefixTree and store the actual key

--- src/PrefixTrees.jl
+++ src/PrefixTrees.jl
@@ -1,14 +1,16 @@
 module PrefixTrees
 
     using IPNets
-    import Base: setindex!, get, getindex, values
-    export PrefixTree, setindex!, subtree, getindex, values, firstparent
+    import Base: setindex!, get, getindex, values, keys, iterate, length
+    export PrefixTree, setindex!, subtree, getindex, values, firstparent, iterate
 
     mutable struct PrefixTree{T}
         vals::Union{Nothing, Vector{T}}
         zero::Union{Nothing, PrefixTree{T}}
         one::Union{Nothing, PrefixTree{T}}
         is_key::Bool
+        key::Union{Nothing, IPNet}
+        len::Int
 
         function PrefixTree{T}() where T
             self = new{T}()
@@ -16,6 +18,8 @@ module PrefixTrees
             self.zero = nothing
             self.one = nothing
             self.is_key = false
+            self.key = nothing
+            self.len = 0
             self
         end
 
@@ -35,6 +39,9 @@ module PrefixTrees
     end
 
 
+    function length(t::PrefixTree{T}) where T
+        t.len
+    end
 
     function setindex!(t::PrefixTree{T}, val::T, key::IPNet) where T
         node = t
@@ -51,7 +58,14 @@ module PrefixTrees
                 node = node.one
             end
         end
+        if node.is_key
+            @assert node.key == key
+        else
+            t.len += 1
+        end
         node.is_key = true
+        node.key = key
+
         if isnothing(node.vals)
             node.vals = [val]
         else
@@ -156,10 +170,91 @@ module PrefixTrees
 
     end
 
-    #function iterate(t::PrefixTree)
 
-    #end
-
-    
+    # we return key -> [vals]
+    function iterate(t::PrefixTree{T}) where T
+        found = false
+        current = t
+        to_check = PrefixTree{T}[]
+        while !found
+            #if length(to_check) > 1
+            #    @debug "in while iterate1, to_check len" length(to_check)
+            #end
+            if current.is_key
+                item = (current.key => current.vals)
+                if !isnothing(current.zero)
+                    #@debug "pushing zero"
+                    push!(to_check, current.zero)
+                end
+                if !isnothing(current.one)
+                    #@debug "pushing one"
+                    push!(to_check, current.one)
+                end
+                #@debug "returning actual item!"
+                return (item, to_check)
+            else
+                #@debug "not a key .."
+                if !isnothing(current.zero)
+                    #@debug "pushing zero"
+                    push!(to_check, current.zero)
+                end
+                if !isnothing(current.one)
+                    #@debug "pushing one"
+                    push!(to_check, current.one)
+                end
+                if isempty(to_check)
+                    return nothing
+                end
+                #@debug "setting current to popfirst!"
+                current = popfirst!(to_check)
+            end
+            #@debug "next while"
+        end
+    end
+    # state::Union{Nothing,Vector{PrefixTree}} ?
+    function iterate(t::PrefixTree{T}, to_check) where T
+        if isnothing(to_check) || isempty(to_check)
+            return nothing
+        end
+        found = false
+        current = popfirst!(to_check)
+        while !found 
+            #if length(to_check) > 1
+            #    @debug "in while iterate2, to_check len" length(to_check)
+            #end
+            if current.is_key
+                #@debug "current.is_key true"
+                item = (current.key => current.vals)
+                if !isnothing(current.zero)
+                    #@debug "pushing zero"
+                    push!(to_check, current.zero)
+                end
+                if !isnothing(current.one)
+                    #@debug "pushing one"
+                    push!(to_check, current.one)
+                end
+                #@debug "returning actual item!"
+                return (item, to_check)
+            else
+                #@debug "not a key .."
+                if !isnothing(current.zero)
+                    #@debug "pushing zero"
+                    push!(to_check, current.zero)
+                end
+                if !isnothing(current.one)
+                    #@debug "pushing one"
+                    push!(to_check, current.one)
+                end
+                if isempty(to_check)
+                    return nothing
+                end
+                #@debug "setting current to popfirst!"
+                current = popfirst!(to_check)
+            end
+            #@debug "next while"
+        end
+        @error "should I end up here?"
+        @debug length(to_check)
+    end
     
 end

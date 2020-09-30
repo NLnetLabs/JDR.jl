3d2d9ac0e47b63c5226fe98a2c9dd63a86c75e9e
Luuk Hendriks <mail@luukhendriks.eu>
2020-09-30 15:22:00 +0200

    keep track of listed but missing files (in both .mft and .cer) in Lookup

--- src/Lookup.jl
+++ src/Lookup.jl
@@ -2,7 +2,8 @@ export search
 
 struct Lookup
     ASNs::Dict{AutSysNum}{Vector{RPKINode}}
-    filenames::Dict{String}{Vector{RPKINode}}
+    filenames::Dict{String}{RPKINode}
+    missing_files::Dict{String}{RPKINode} # split up between CER and MFT?
     prefix_tree::PrefixTree{RPKINode}
     pubpoints::Dict{String}{RPKINode}
     too_specific::Set{RPKINode}
@@ -10,14 +11,17 @@ struct Lookup
     invalid_certs::Set{RPKINode}
     valid_certs::Set{RPKINode}
 end
-Lookup() = Lookup(Dict(), Dict(), PrefixTree{RPKINode}(), Dict(), Set(), Set(), Set(), Set())
+Lookup() = Lookup(Dict(), Dict(), Dict(), PrefixTree{RPKINode}(), Dict(), Set(), Set(), Set(), Set())
 
-function search(l::Lookup, asn::AutSysNum)
-    if asn in keys(l.ASNs)
-        l.ASNs[asn]
-    else
-        []
-    end
+function add_filename!(l::Lookup, fn::String, node::RPKINode)
+    l.filenames[fn] = node
+end
+function add_missing_file!(l::Lookup, fn::String, node::RPKINode)
+    l.missing_files[fn] = node
+end
+
+function search(l::Lookup, asn::AutSysNum) :: Vector{RPKINode}
+    get(l.ASNs, asn, AutSysNum[])
 end
 
 # FIXME: do we need separate lookup trees for v6 and v4?
@@ -29,23 +33,15 @@ function search(l::Lookup, prefix::IPv6Net)
     values(firstparent(l.prefix_tree, prefix))
 end
 
-# TODO make a struct, e.g. ObjectFilename, for this?
-function search(l::Lookup, filename::String) :: Dict{String}{Vector{RPKINode}}
+function search(l::Lookup, filename::String) :: Dict{String}{RPKINode}
     filter(fn->occursin(filename, first(fn)), l.filenames)
-    #get(l.filenames, filename, 
-    #if filename in keys(l.filenames)
-    #    res = l.filenames[filename]
-    #    @assert length(res) == 1
-    #    first(res)
-    #else
-    #    nothing
-    #end
 end
 
 function Base.show(io::IO, l::Lookup)
     println(io, "filenames: ", length(l.filenames))
-    println(io, "ASNs: ", length(keys(l.ASNs)))
+    println(io, "missing files: ", length(l.missing_files))
     println(io, "pubpoints: ", length(l.pubpoints))
+    println(io, "ASNs: ", length(keys(l.ASNs)))
     println(io, "too_specific: ", length(l.too_specific))
     println(io, "invalid_signatures: ", length(l.invalid_signatures))
     println(io, "invalid_certs: ", length(l.invalid_certs))

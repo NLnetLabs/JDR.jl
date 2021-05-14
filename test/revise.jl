7d19f7003c10663656451d3432efd8bc88488655
Luuk Hendriks <mail@luukhendriks.eu>
2021-05-14 09:45:46 +0200

    Move dependencies for test to specific target, move jive/revise setup out of runtests.jl

---
+++ test/revise.jl
@@ -0,0 +1,26 @@
+using Revise, Jive
+using JDR
+using Test
+using Glob
+
+function all_rpki_files(dir, exts=["cer", "mft", "crl", "roa"])
+    all_files = []
+    for d in walkdir(dir), ext in exts
+        Base.append!(all_files, glob("*.$(ext)", d[1]))
+    end
+    all_files
+end
+
+
+trigger = function (path)
+    printstyled("changed ", color=:cyan)
+    println(path)
+    sleep(0.1)
+    revise()
+    runtests(@__DIR__, skip=["runtests.jl"])
+end
+
+watch(trigger, @__DIR__, sources=[pathof(JDR)])
+trigger("")
+
+Base.JLOptions().isinteractive==0 && wait()
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,5 @@
-using Revise, Jive
 using JDR
-using Test
-using Glob
+using Jive, Test
 
 ROUTINATOR_DIR = "/home/luuk/.rpki-cache/repository/rsync/"
 TESTDATA = joinpath(dirname(pathof(JDR)), "..", "test", "testdata")
@@ -10,24 +8,5 @@ TESTDATA = joinpath(dirname(pathof(JDR)), "..", "test", "testdata")
 # some helpers:
 testdata_fn(filename::String) = joinpath(dirname(pathof(JDR)), "..", "test", "testdata", filename)
 
-function all_rpki_files(dir, exts=["cer", "mft", "crl", "roa"])
-    all_files = []
-    for d in walkdir(dir), ext in exts
-        Base.append!(all_files, glob("*.$(ext)", d[1]))
-    end
-    all_files
-end
 
-
-trigger = function (path)
-    printstyled("changed ", color=:cyan)
-    println(path)
-    sleep(0.1)
-    revise()
-    runtests(@__DIR__, skip=["runtests.jl"])
-end
-
-watch(trigger, @__DIR__, sources=[pathof(JDR)])
-trigger("")
-
-Base.JLOptions().isinteractive==0 && wait()
+runtests(@__DIR__, skip=["revise.jl"])
